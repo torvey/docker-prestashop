35dae1869e8ef14fb19b977a36ccaa20c593100b
Radosław Śmidoda <radek.smidoda@gmail.com>
2022-01-09 20:10:36 +0100

    turn off https

--- copy-from.bat
+++ copy-from.bat
@@ -1,4 +1,4 @@
 @echo off
 
 docker cp docker-prestashop_prestashop_1:/var/www/html .
-docker exec -it docker-prestashop_mariadb_1 mysqldump -u root -proot prestashop > ./docker/mariadb/prestashop.sql  
+docker exec -it docker-prestashop_mysql_1 mysqldump -u root -proot prestashop > ./docker/mariadb/prestashop.sql  
