55e1dc999d03f57ee4aa80f42191455e7103ffdd
Radosław Śmidoda <radek.smidoda@gmail.com>
2022-01-12 20:09:16 +0100

    docker compose change

--- db/script.sh
+++ db/script.sh
@@ -4,4 +4,4 @@ mysql -pstudent -e "CREATE DATABASE IF NOT EXISTS be_181291;"
 mysql -pstudent -e "CREATE USER IF NOT EXISTS be_181291@'%' IDENTIFIED BY 'politechnika123';"
 mysql -pstudent -e "GRANT ALL PRIVILEGES ON be_181291.* TO be_181291@'%';"
 mysql -pstudent -e "FLUSH PRIVILEGES;"
-mysql -u be_181291 -ppolitechnika123 be_181291 < /tmp/prestashop.sql
+mysql -u be_181291 -ppolitechnika123 be_181291 < /tmp/be_181291.sql
